b29a1fdf8a5322c6381a8026ebdcda82df0e25ba
Dio_V <diov87@outlook.com>
2017-03-08 14:51:06 +0800

    add video fetcher

--- robot.py
+++ robot.py
@@ -5,14 +5,15 @@ import requests
 from werobot import WeRoBot
 from lxml import html
 
-robot = WeRoBot(token='x_forlunch', enable_session=True)
-scale_pattern = re.compile(r's\d+x\d+/')
-cache_pattern = re.compile(r'\?ig_cache_key.*$')
+robot = WeRoBot(token="x_forlunch", enable_session=True)
+scale_pattern = re.compile(r"s\d+x\d+/")
+cache_pattern = re.compile(r"\?ig_cache_key.*$")
+type_xpath = "//meta[@property='og:type']/@content"
 
 
 @robot.subscribe
 def subscriber(message):
-    return '看个电影，听个歌，美滋滋~'
+    return "愿你永远有一颗有趣的心~❤️"
 
 
 @robot.filter(re.compile(r".*?instagram.com/p.*?"))
@@ -21,20 +22,22 @@ def get_ins_pic(message):
     req = requests.get(url)
     if req.status_code == 200:
         tree = html.fromstring(req.text)
-        url = tree.xpath("//meta[@property='og:image']/@content")[0]
-        if url:
-            if scale_pattern.search(url):
-                url = url.replace(scale_pattern.search(url).group(0), '')
-            if cache_pattern.search(url):
-                url = url.replace(cache_pattern.search(url).group(0), '')
+        ins_type = tree.xpath(type_xpath)[0]
+        if ins_type:
+            url = tree.xpath("//meta[@property='og:{type}']/@content".format(type=ins_type))[0]
+            if url and ins_type == "image":
+                if scale_pattern.search(url):
+                    url = url.replace(scale_pattern.search(url).group(0), '')
+                if cache_pattern.search(url):
+                    url = url.replace(cache_pattern.search(url).group(0), '')
 
             return url
         else:
-            return 'There is no image return'
+            return "你输入的可能是假的 instagram 网址😝"
 
 
 @robot.handler
 def hello(message, session):
     count = session.get("count", 0) + 1
     session["count"] = count
-    return '嘘~别说话~'
+    return "🐶汪！汪汪汪~"
